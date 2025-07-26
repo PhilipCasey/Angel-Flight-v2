99e1c5f33f2ecb9862594c2b4c9002b76938effe
Philip Casey <philip@philipcaseyphoto.com>
2025-07-26 14:40:26 -0400

    Initial Commit

---
+++ Angel Flight v2/Angel_Flight_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Angel_Flight_v2App.swift
+//  Angel Flight v2
+//
+//  Created by Philip Casey on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Angel_Flight_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
