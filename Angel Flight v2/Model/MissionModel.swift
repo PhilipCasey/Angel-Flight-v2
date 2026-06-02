5119a1d0f509c1b01e6ded0510a2022f6f666245
Philip Casey <philip@philipcaseyphoto.com>
2026-06-02 11:51:30 -0400

    Updated MissionModel with new json structure

--- Angel Flight v2/Model/MissionModel.swift
+++ Angel Flight v2/Model/MissionModel.swift
@@ -5,43 +5,127 @@
 //  Created by Philip Casey on 7/28/25.
 //
 
-import SwiftUI
+import Foundation
 
-struct Mission: Codable, Hashable {
+struct Mission: Identifiable, Codable {
+    let id: String
+    let status: MissionStatus
+    let patient: Patient
+    let mission: MissionInfo
+    let route: Route
+    let passenger: Passenger
+    let baggage: Baggage
+    let flightTotals: FlightTotals?
+    let expenses: Expenses?
+    let comments: String?
     
-        var id: String? = nil
-        var date: String? = nil
-        var dayOfWeek: String? = nil
-        var departureTime: String? = nil
-        var departureCity: String? = nil
-        var departureAirport: String? = nil
-        var destinationCity: String? = nil
-        var destinationAirport: String? = nil
-        var patientCare: String? = nil
-        var patientAge: String? = nil
-        var patientWeight: String? = nil
-        var passengerWeight: String? = nil
-        var baggageWeight: String? = nil
-
     var totalWeightText: String {
-        let weights = [patientWeight, passengerWeight, baggageWeight]
-        let expectedCount = weights.filter { $0 != nil && $0 != "N/A" }.count
-        let parsedWeights = weights.compactMap { weight -> Int? in
-            guard let weight, weight != "N/A" else {
-                return nil
-            }
-
-            return Int(weight)
+        let weights = [
+            patient.weight,
+            passenger.weight,
+            baggage.weight
+        ]
+        // creates array of integers rather than an array of optional integers, adds them
+        //map() will take a value out of its container, transform it using the code you specify, then put it back in its container. compactMap() does the same thing, but if your transformation returns an optional it will be unwrapped and have any nil values discarded.
+        let totalWeight = weights.compactMap { $0 }.reduce(0, +)
+        
+        guard totalWeight > 0 else {
+            return "N/A"
         }
 
-        guard parsedWeights.count == expectedCount else {
-            return "loading"
-        }
-
-        return String(parsedWeights.reduce(0, +))
+        return String(Int(totalWeight))
     }
     
-    static let sampleMission = Mission(
+    static let sampleMissionAvailable = Mission(
+        id: "25-0625-01",
+        status: .available,
+        patient: Patient.init(
+            name: nil,
+            care: "Transplant-Heart",
+            age: 85,
+            weight: 230,
+        ),
+        mission: MissionInfo.init(
+            date: "July 28, 2025",
+            dayOfWeek: "Monday",
+            departureTime: "8:00 am",
+            completedDate: nil,
+            completedDayOfWeek: nil
+        ),
+        route: Route.init(
+            departure: AirportLocation.init(
+                city: "Savannah",
+                state: "GA",
+                airport: "SAV"),
+            destination: AirportLocation.init(
+                city: "Atlanta",
+                state: "GA",
+                airport: "PDK")
+        ),
+        passenger: Passenger.init(
+            weight: 146,
+        ),
+        baggage: Baggage.init(
+            weight: 25
+        ),
+        flightTotals: nil,
+        expenses: nil,
+        comments: nil
+    )
+}
+
+enum MissionStatus: String, Codable {
+    case completed
+    case accepted
+    case available
+}
+
+struct Patient: Codable {
+    let name: String?
+    let care: String?
+    let age: Int?
+    let weight: Double?
+}
+
+struct MissionInfo: Codable {
+    let date: String
+    let dayOfWeek: String
+    let departureTime: String?
+    let completedDate: String?
+    let completedDayOfWeek: String?
+}
+
+struct Route: Codable {
+    let departure: AirportLocation
+    let destination: AirportLocation
+}
+
+struct AirportLocation: Codable {
+    let city: String
+    let state: String
+    let airport: String
+}
+
+struct Passenger: Codable {
+    let weight: Double?
+}
+struct Baggage: Codable {
+    let weight: Double?
+}
+
+struct FlightTotals: Codable {
+    let hoursFlown: Double
+    let milesFlown: Double
+}
+
+struct Expenses: Codable {
+    let hourlyOperatingCost: Double
+    let additionalExpenses: Double
+    let description: String?
+}
+
+    /*
+    static let sampleMission = Mission (
         id: "25-0625-01",
         date: "July 28, 2025",
         dayOfWeek: "Monday",
@@ -56,5 +140,8 @@ struct Mission: Codable, Hashable {
         passengerWeight: "146",
         baggageWeight: "10"
     )
+*/
 
-}
+
+
+
